7d371edc38f82ad2d0c7d5352964bbb9b971e52d
Ahmed Bentaj Hamyani <ahmedbentaj710@gmail.com>
2024-04-02 20:30:12 +0000

    add product details

---
+++ src/app/products/components/product-details.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from "@angular/common";
+import { Component, Inject, Input } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
+import { Product } from "src/app/models/product.model";
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+    selector: 'fk-product-details',
+    standalone: true,
+    imports: [
+        MatDialogModule,
+        CommonModule,
+        MatIconModule
+    ],
+    template: `
+        <button mat-dialog-close class="flex justify-end w-full p-2 outline-none">
+            <mat-icon aria-hidden="false" aria-label="Example close icon" fontIcon="close" class="text-2xl"></mat-icon>
+        </button>
+        <mat-dialog-content class="mat-typography">
+            <div class="grid grid-cols-1 lg:grid-cols-2 place-items-center gap-x-8 mt-6 text-gray-900">
+                <img src={{product.image}} alt='' class='h-[300px] md:h-[400px]' />
+                <div class="flex flex-grow flex-col justify-between bg-gray-100 px-5 py-5 w-full rounded-lg">
+                    <p class='font-semibold'>{{product.category}}</p>
+
+                    <div class="flex justify-between items-start space-x-3 font-bold">
+                        <p class='text-lg md:text-2xl'>{{product.title}}</p>
+                        <p class='md:text-xl whitespace-nowrap'>
+                            {{ product.price | currency:'USD':'symbol':'1.2-2' }}
+                        </p>
+                    </div>
+
+                    <p class='text-sm md:text-base mt-3'>{{product.description}}</p>
+                    
+                    <button mat-raised-button mat-dialog-close
+                        class="w-full bg-teal-500 text-white shadow-md mt-3 p-2.5 rounded-md hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 transition-all ease-in-out duration-300">Add to cart</button>
+                </div>
+            </div>
+        </mat-dialog-content>
+    `
+})
+export class ProductDetailsComponent {
+    constructor(@Inject(MAT_DIALOG_DATA) public product: Product) { }
+}
