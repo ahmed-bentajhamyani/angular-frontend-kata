b718af02e027dcbb2672e59585c4e27eb1b85ea5
Ahmed Bentaj Hamyani <ahmedbentaj710@gmail.com>
2024-04-22 09:50:38 +0100

    feat: change done icon and add open cart when click on the snack bar

--- src/app/products/components/snack_bar/added-to-cart-snack-bar.component.ts
+++ src/app/products/components/snack_bar/added-to-cart-snack-bar.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { MatIconModule } from "@angular/material/icon";
 import { MatSnackBarRef } from "@angular/material/snack-bar";
+import { CartComponent } from "src/app/components/cart/cart.component";
 
 @Component({
     selector: 'added-to-cart-snack-bar',
     template: `
-        <span class="flex items-center text-text-primary font-medium text-base">
-            <mat-icon aria-hidden="false" aria-label="Example done icon" fontIcon="done" class="text-primary"></mat-icon> Added to cart
+        <span class="flex items-center text-text-primary font-medium text-base cursor-pointer" (click)="openCart()">
+            <mat-icon aria-hidden="false" aria-label="Example check_circle icon" fontIcon="check_circle" class="text-primary"></mat-icon>  Added to cart
         </span>
     `,
     standalone: true,
@@ -14,4 +16,11 @@ import { MatSnackBarRef } from "@angular/material/snack-bar";
 })
 export class AddedToCartSnackBarComponent {
     snackBarRef = inject(MatSnackBarRef);
+    public readonly dialog = inject(MatDialog);
+
+    openCart() {
+        const dialogRef = this.dialog.open(CartComponent);
+
+        dialogRef.afterClosed().subscribe();
+    }
 }
