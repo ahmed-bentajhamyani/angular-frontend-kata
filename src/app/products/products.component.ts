5b4cbcfe097ba2aae18e1a2b8e13cd62c63dc610
Ahmed Bentaj Hamyani <ahmedbentaj710@gmail.com>
2024-04-22 10:29:47 +0100

    feat: add template if no product found

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from "@angular/core";
-import { AsyncPipe, NgFor } from "@angular/common";
+import { AsyncPipe, NgFor, NgTemplateOutlet } from "@angular/common";
 import { ProductService } from "../services/product.service";
 import { Product } from "../models/product.model";
 import { ProductCardComponent } from "./components/product-card.component";
@@ -10,17 +10,26 @@ import { Observable, combineLatest, map, of } from "rxjs";
     standalone: true,
     imports: [
         NgFor,
+        NgTemplateOutlet,
         ProductCardComponent,
         AsyncPipe
     ],
     template: `
-        <div class="mb-16 w-full">
+        <ng-template #empty>
+            <div class="flex justify-center items-center w-full h-full mt-72 xl:mt-80">
+                <p class="text-xl text-center">No product found</p>
+            </div>
+        </ng-template>
+
+        <ng-template #content class="mb-16 w-full">
             <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4">
                 <ng-container *ngFor="let product of (products$ | async)">
                     <fk-product-card [product]="product"></fk-product-card>
                 </ng-container>
             </div>
-        </div>
+        </ng-template>
+
+        <ng-container *ngTemplateOutlet="(products$ | async)?.length! > 0 ? content : empty"></ng-container>
     `,
 })
 export class ProductsComponent {
